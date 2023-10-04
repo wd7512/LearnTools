6ff9f43040c4c98d6fc9fff6adfde8f4b1cf328b
William Dennis <wwdennis.home@gmail.com>
2023-10-04 11:44:45 +0100

    Version 0.0.1
    Initial Implementation

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# hwlearn
+# LearnTools
 
 ### Note to Self
+ - Setup Venv
+    - install wheel
  - To upload package
  - build dist:
     - python setup.py sdist bdist_wheel
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2' 
+VERSION = '0.0.1' 
 DESCRIPTION = 'My first Python package'
 LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="hwlearn", 
+        name="learntools", 
         version=VERSION,
         author="William Dennis",
         author_email="wwdennis.home@gmail.com",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=['hwlearn','numpy'], # add any additional packages that 
+        install_requires=['learntools','numpy'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
-        keywords=['python', 'first package'],
+        keywords=['python', 'neural network','learning'],
         
 )
