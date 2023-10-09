2a2ec1301455f96f66b5890695eddcdc8b4ddbca
William Dennis <wwdennis.home@gmail.com>
2023-10-09 11:39:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.7' 
+VERSION = '0.0.8' 
 DESCRIPTION = 'My first Python package'
 LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 
