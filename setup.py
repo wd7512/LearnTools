1e0d305a80e8a30a2f749fdb7a936cfb9a01dda4
William Dennis <wwdennis.home@gmail.com>
2023-11-20 08:42:58 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.17' 
+VERSION = '0.0.18' 
 DESCRIPTION = 'Reinforcement Learning with Numpy'
 LONG_DESCRIPTION = 'Reinforcement Learning with Numpy'
 
